41f06364518e5b860f82b5c5758318fefa87701a
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-11-01 11:50:14 +0700

    first commit

---
+++ src/Programmer/bagus/iyo/classes/StringApp.java
@@ -0,0 +1,13 @@
+package Programmer.bagus.iyo.classes;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Bgaus iyo";
+        String nameLowecase = name.toLowerCase();
+
+        System.out.println(name);
+        System.out.println(nameLowecase);
+        System.out.println(name.length());
+
+    }
+}
